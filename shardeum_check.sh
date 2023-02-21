5969cc343d0a85b46740952086f9193b8b133244
TianYuan <95566315+ty01528@users.noreply.github.com>
2023-02-21 16:14:22 +0800

    Add files via upload

--- shardeum_check.sh
+++ shardeum_check.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-echo "START SHARDEUM_CHECK"
 while :
 do
 docker exec shardeum-dashboard operator-cli status | grep stopped
-if [ $? -eq 0 ]; then
+if [ $? -eq 0 ]
+then
         echo $(date)
         echo "START SHARDEUM"
         docker exec shardeum-dashboard operator-cli start
